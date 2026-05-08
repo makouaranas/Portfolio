630612885d88c2992a1335007ea88b6057fe3b1f
ANAS MAKOUAR <118914175+makouaranas@users.noreply.github.com>
2026-05-08 12:55:00 +0100

    Fix message in root endpoint to remove extra exclamation mark

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Backend CI/CD is working!!"}
+    return {"message": "Backend CI/CD is working!"}
 
 @app.get("/api/health")
 def health():
